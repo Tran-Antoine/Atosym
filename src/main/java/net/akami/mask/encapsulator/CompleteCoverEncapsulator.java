55948a83da8e15ed9e610197d7f4ece786d74838
Askigh <antoine2001.tran@gmail.com>
2019-05-20 23:34:00 +0200

    javadoc

--- src/main/java/net/akami/mask/encapsulator/CompleteCoverEncapsulator.java
+++
@@ -1,4 +0,0 @@
-package net.akami.mask.encapsulator;
-
-public interface CompleteCoverEncapsulator extends ExpressionEncapsulator {
-}
--- src/main/java/net/akami/mask/handler/IODefaultFormatter.java
+++
@@ -1,7 +0,0 @@
-package net.akami.mask.handler;
-
-public interface IODefaultFormatter<T> {
-
-    T inFormat(T input);
-    T outFormat(T input);
-}
